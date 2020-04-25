6d10aa7c40e205c2725dd64f31437cb45c827824
Dominik Stefancik <dstefancik@gmail.com>
2020-04-25 17:47:27 +0200

    Add styled component for Stage

--- src/components/stage/Stage.tsx
+++ src/components/stage/Stage.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from "react";
 import Cell from "../cell/Cell";
+import { NoShape } from "../../models/tetromino";
+import { StyledStage } from "./StyledStage";
 
 const Stage: FC<{ stage: any[] }> = ({ stage }) => {
   return (
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
       {stage.map((row) => {
         return row.map((cell, index) => {
-          return <Cell key={index} />;
+          return <Cell key={index} type={NoShape} />;
         });
       })}
-    </div>
+    </StyledStage>
   );
 };
 
