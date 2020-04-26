2399d7f3ff65191726bf73b52a8206e60f67edd3
Dominik Stefancik <dstefancik@gmail.com>
2020-04-26 21:23:30 +0200

    Add function for removing full rows

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -6,8 +6,33 @@ import { CellState, EMPTY_CELL } from "../models/cell";
 // custom hook for manipulating the stage
 export const useStage = (tetrominoState: TetrominoState, resetTetrominoState) => {
   const [stage, setStage] = useState(createStage());
+  const [numberOfRowsCleared, setNumberOfRowsCleared] = useState(0);
 
   useEffect(() => {
+    setNumberOfRowsCleared(0);
+
+    // this function gets a new updated stage and checks if it contains full rows
+    // then it returns a new stage without full rows
+    const sweepFullRows = (newStage) => {
+      return newStage.reduce((accumulator, row) => {
+        const isRowFull: boolean = row.reduce((containsNoZero, cell) => {
+          return containsNoZero && cell[0] !== 0;
+        }, true);
+
+        // if the row is full, we will not push it into the accumulator array
+        // instead we will add an empty row on the top of the accumulator
+        if (isRowFull) {
+          setNumberOfRowsCleared((previous) => previous + 1);
+          accumulator.unshift(new Array(newStage[0].length).fill(EMPTY_CELL));
+        } else {
+          // row is not full, push it as it is into the accumulator
+          accumulator.push(row);
+        }
+
+        return accumulator;
+      }, []);
+    };
+
     const updateStage = (previousStage) => {
       // First, get rid of everything which is on stage
       const newStage = previousStage.map((row) =>
@@ -28,6 +53,7 @@ export const useStage = (tetrominoState: TetrominoState, resetTetrominoState) =>
 
       if (tetrominoState.collided) {
         resetTetrominoState();
+        return sweepFullRows(newStage);
       }
 
       return newStage;
@@ -36,5 +62,5 @@ export const useStage = (tetrominoState: TetrominoState, resetTetrominoState) =>
     setStage((previousStage) => updateStage(previousStage));
   }, [tetrominoState, resetTetrominoState]);
 
-  return [stage, setStage];
+  return [stage, setStage, numberOfRowsCleared];
 };
