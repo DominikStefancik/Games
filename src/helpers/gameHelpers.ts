bdc30fc94407853046e88340987d1eab9a5fd4d3
Dominik Stefancik <dstefancik@gmail.com>
2020-04-26 16:05:25 +0200

    Add function for checking collisions

--- src/helpers/gameHelpers.ts
+++ src/helpers/gameHelpers.ts
@@ -1,5 +1,5 @@
-import { Tetromino, TETROMINOS } from "../models/tetromino";
-import { EMPTY_CELL } from "../models/cell";
+import { Tetromino, TETROMINOS, TetrominoState } from "../models/tetromino";
+import { CellState, EMPTY_CELL } from "../models/cell";
 
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
@@ -17,4 +17,28 @@ export const getRandomTetromino = (): Tetromino => {
   const randomKey = tetrominosKeys[index];
 
   return TETROMINOS[randomKey];
+};
+
+export const checkCollision = (tetrominoState: TetrominoState, stage, moveX, moveY): boolean => {
+  for (let y = 0; y < tetrominoState.shape.length; y++) {
+    for (let x = 0; x < tetrominoState.shape[y].length; x++) {
+      // 1. Check if we are at thew cell which renders a tetromino's block
+      if (tetrominoState.shape[y][x] !== 0) {
+        if (
+          // 2. Check that our move is inside the game areas height (y)
+          // We need to check if we are at the bottom of the stage
+          !stage[y + tetrominoState.position.y + moveY] ||
+          // 3. Check that our move is inside the game areas width (x)
+          !stage[y + tetrominoState.position.y + moveY][x + tetrominoState.position.x + moveX] ||
+          // 4. Check if the cell we are moving towards is set to 'clear', otherwise we are colliding with other blocks
+          stage[y + tetrominoState.position.y + moveY][x + tetrominoState.position.x + moveX][1] ===
+            CellState.MERGED
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
 };
