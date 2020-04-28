7fb3d3c5de68bcfa30f1a05edd804e702e0174df
Dominik Stefancik <dstefancik@gmail.com>
2020-04-28 16:18:33 +0200

    Define constants

--- pong-with-javafx/src/main/java/com/nortoncommander/App.java
+++ pong-with-javafx/src/main/java/com/nortoncommander/App.java
@@ -1,22 +1,34 @@
 package com.nortoncommander;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import static com.nortoncommander.util.Constants.STAGE_HEIGHT;
+import static com.nortoncommander.util.Constants.STAGE_WIDTH;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
+    private double ballPositionX = STAGE_WIDTH / 2;
+    private double ballPositionY = STAGE_HEIGHT / 2;
+    private int ballSpeedX = 1;
+    private int ballSpeedY = 1;
+
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+
+    private double playerOnePositionX = 0;
+    private double playerOnePositionY = STAGE_HEIGHT / 2;
+    private double playerTwoPositionX = 0;
+    private double playerTwoPositionY = STAGE_HEIGHT / 2;
+
+    private boolean hasGameStarted = false;
+
     @Override
     public void start(Stage stage) {
 
-        var label = new Label("Hello World in JavaFX.");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
         stage.show();
     }
 
---
+++ pong-with-javafx/src/main/java/com/nortoncommander/util/Constants.java
@@ -0,0 +1,9 @@
+package com.nortoncommander.util;
+
+public class Constants {
+  public static final int STAGE_WIDTH = 800;
+  public static final int STAGE_HEIGHT = 600;
+  public static final int PLAYER_WIDTH = 100;
+  public static final int PLAYER_HEIGHT = 15;
+  public static final int BALL_RADIUS = 15;
+}
