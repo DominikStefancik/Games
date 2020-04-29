c3a0fc136706ecdcd63a2d1f075ec1c326c5e2d9
Dominik Stefancik <dstefancik@gmail.com>
2020-04-29 23:43:49 +0200

    Create canvas and paddle in it

---
+++ breakout-with-javascript/game.js
@@ -0,0 +1,37 @@
+// Game constants
+const PADDLE_WIDTH = 100;
+const PADDLE_HEIGHT = 20;
+const PADDLE_MARGIN_BOTTOM = 50;
+
+const canvas = document.querySelector("#gameCanvas");
+// context of the canvas
+const context = canvas.getContext("2d");
+
+// Set the borders of the canvas
+canvas.style.border = "1px solid #0ff";
+// Make lines stronger when drawing to the canvas
+context.lineWidth = 3;
+
+// Paddle object
+const paddle = {
+  x: canvas.width / 2 - PADDLE_WIDTH / 2,
+  y: canvas.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+  width: PADDLE_WIDTH,
+  height: PADDLE_HEIGHT,
+  deltaX: 5,
+};
+
+const drawPaddle = () => {
+  const { x, y, width, height } = paddle;
+
+  console.log(x, y, width, height);
+
+  context.fillStyle = "#2e3548";
+  context.fillRect(x, y, width, height);
+
+  // draw the border of the paddle
+  context.strokeStyle = "#ffcd05";
+  context.strokeRect(x, y, width, height);
+};
+
+drawPaddle();
