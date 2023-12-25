460124d7660c8542ad10d5066ff173fb41ebdf0a
Dominik Stefancik <dstefancik@gmail.com>
2023-12-25 15:02:25 +0100

    Set up audio

---
+++ space-invaders-simple-with-rust/src/audio_sound.rs
@@ -0,0 +1,21 @@
+pub enum AudioSound {
+    Explode,
+    Lose,
+    Move,
+    Pew,
+    Startup,
+    Win
+}
+
+impl AsRef<str> for AudioSound {
+    fn as_ref(&self) -> &str {
+        match self {
+            AudioSound::Explode => "Explode",
+            AudioSound::Lose => "Lose",
+            AudioSound::Move => "Move",
+            AudioSound::Pew => "Pew",
+            AudioSound::Startup => "Startup",
+            AudioSound::Win => "Win",
+        }
+    }
+}
--- space-invaders-simple-with-rust/src/main.rs
+++ space-invaders-simple-with-rust/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod audio_sound;
+
+use std::error::Error;
+use rusty_audio::Audio;
+use audio_sound::AudioSound;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    // add all audio sources to the audio manager
+    audio.add(AudioSound::Explode, "./audio/explode.wav");
+    audio.add(AudioSound::Lose, "./audio/lose.wav");
+    audio.add(AudioSound::Move, "./audio/move.wav");
+    audio.add(AudioSound::Pew, "./audio/pew.wav");
+    audio.add(AudioSound::Startup, "./audio/startup.wav");
+    audio.add(AudioSound::Win, "./audio/win.wav");
+
+    // the sound is played in a separate thread
+    audio.play(AudioSound::Startup);
+    audio.wait(); // wait until all audio is done playing
+
+    Ok(())
 }
