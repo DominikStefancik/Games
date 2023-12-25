616c86a19370ed94dbdb76f8244fbaa115811eb2
Dominik Stefancik <dstefancik@gmail.com>
2023-12-25 15:43:43 +0100

    Set up a terminal

--- space-invaders-simple-with-rust/src/main.rs
+++ space-invaders-simple-with-rust/src/main.rs
@@ -1,10 +1,27 @@
 mod audio_sound;
 
 use std::error::Error;
+use std::io;
+use std::io::Stdout;
+use crossterm::{ExecutableCommand, terminal};
+use crossterm::cursor::{Hide, Show};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
 use audio_sound::AudioSound;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = initialise_audio();
+    let mut stdout = initialise_terminal().expect("Error while initialising terminal");
+
+    // the sound is played in a separate thread
+    audio.play(AudioSound::Startup);
+
+    clean_up_resources(audio, &mut stdout)?;
+
+    Ok(())
+}
+
+fn initialise_audio() -> Audio {
     let mut audio = Audio::new();
     // add all audio sources to the audio manager
     audio.add(AudioSound::Explode, "./audio/explode.wav");
@@ -14,9 +31,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add(AudioSound::Startup, "./audio/startup.wav");
     audio.add(AudioSound::Win, "./audio/win.wav");
 
-    // the sound is played in a separate thread
-    audio.play(AudioSound::Startup);
+    audio
+}
+
+fn initialise_terminal() -> Result<Stdout, io::Error> {
+    let mut stdout = io::stdout();
+    // enable raw mode so we can get a keyboard input as it occurs
+    terminal::enable_raw_mode()?;
+    // let's enter our alternate screen
+    // crossterm package extends the stdout with "execute" method
+    stdout.execute(EnterAlternateScreen)?;
+    // hide the cursor
+    stdout.execute(Hide)?;
+
+    Ok(stdout)
+}
+
+fn clean_up_resources(audio: Audio, stdout: &mut Stdout) -> Result<(), io::Error> {
     audio.wait(); // wait until all audio is done playing
+    // show the terminal screen
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 
     Ok(())
 }
