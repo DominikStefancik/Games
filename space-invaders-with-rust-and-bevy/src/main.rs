48f548be817e76f77352d34df1af61ab95b7914e
Dominik Stefancik <dstefancik@gmail.com>
2024-01-07 17:38:59 +0100

    Setup the game window

--- space-invaders-with-rust-and-bevy/src/main.rs
+++ space-invaders-with-rust-and-bevy/src/main.rs
@@ -1,3 +1,23 @@
+// use bevy::prelude::*;
+
+use bevy::DefaultPlugins;
+use bevy::prelude::{App, ClearColor, Color, PluginGroup, Window, WindowPlugin};
+
+const PLAYER_SPRITE: &str = "player_a.png";
+
+// Bevy has 4 main constructs:  Entity, Component, System, Resource
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Rust Space Invaders".to_string(),
+                // property resolution is of the type WindowResolution
+                // and the method "into()" automatically converts a tuple into this required type
+                resolution: (598_f32, 676_f32).into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .run();
 }
