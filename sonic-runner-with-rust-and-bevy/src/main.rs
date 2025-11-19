f272f2fae2d0b5f8ce639702c929a8f02af2b643
Dominik Stefancik <dstefancik@gmail.com>
2025-11-19 17:03:09 +0100

    Customise the window properties

--- sonic-runner-with-rust-and-bevy/src/main.rs
+++ sonic-runner-with-rust-and-bevy/src/main.rs
@@ -1,5 +1,7 @@
 use bevy::app::App;
 
+mod plugins;
+
 fn main() {
-    App::new().run()
+    App::new().add_plugins(plugins::default::plugin).run();
 }
---
+++ sonic-runner-with-rust-and-bevy/src/plugins/default.rs
@@ -0,0 +1,25 @@
+use std::num::NonZero;
+
+use bevy::{
+    DefaultPlugins,
+    app::{App, PluginGroup},
+    window::{Window, WindowPlugin, WindowResolution},
+};
+
+const WINDOW_RESOLUTION: (u32, u32) = (1400, 900);
+
+pub fn plugin(app: &mut App) {
+    let primary_window = Window {
+        title: "Sonic Runner".into(),
+        resizable: false,
+        resolution: WindowResolution::new(WINDOW_RESOLUTION.0, WINDOW_RESOLUTION.1),
+        canvas: Some("#bevy".to_owned()),
+        desired_maximum_frame_latency: NonZero::new(1u32),
+        ..Default::default()
+    };
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(primary_window),
+        ..Default::default()
+    }));
+}
