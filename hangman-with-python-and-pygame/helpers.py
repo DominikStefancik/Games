7ad7cdf379e4950de08b98d7853a34f5f2babc39
Dominik Stefancik <dstefancik@gmail.com>
2026-01-21 17:41:15 +0100

    Implement drawing letters

--- hangman-with-python-and-pygame/helpers.py
+++ hangman-with-python-and-pygame/helpers.py
@@ -1,2 +1,22 @@
+import pygame
+
+from constants import BLACK, BUTTON_CIRCLE_THICKNESS, BUTTON_RADIUS, WHITE
+
 def get_hangman_image_path(index):
     return f"assets/images/hangman_{index}.png"
+
+
+def draw(surface, hangman_images, hangman_status, letters, font):
+    # Fill the background of the game window
+    surface.fill(WHITE)
+    surface.blit(hangman_images[int(hangman_status)], (150, 100))
+
+    # Draw letters
+    for letter_tuple in letters:
+        character, x, y = letter_tuple
+        pygame.draw.circle(surface, BLACK, (x, y), BUTTON_RADIUS, BUTTON_CIRCLE_THICKNESS)
+        text = font.render(character, 1, BLACK)
+        surface.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
+
+    # In Pygame we have to manually say when we want to display on the screen everything we drew so far
+    pygame.display.update()
--- hangman-with-python-and-pygame/main.py
+++ hangman-with-python-and-pygame/main.py
@@ -1,8 +1,8 @@
 import pygame
 
-from constants import FPS, WHITE, WINDOW_HEIGHT, WINDOW_WIDTH
+from constants import BUTTON_GAP_SIZE, BUTTONS_IN_ROW, BUTTON_RADIUS, BUTTON_ROWS, FPS, LETTER_A_ASCII_CODE, WINDOW_HEIGHT, WINDOW_WIDTH
 from hangman_status import HangmanStatus
-from helpers import get_hangman_image_path
+from helpers import draw, get_hangman_image_path
 
 pygame.init()
 
@@ -19,8 +19,22 @@ for index in range(7):
     image = pygame.image.load(image_path)
     hangman_images.append(image)
 
+# Load fonts
+LETTER_FONT = pygame.font.SysFont('comicsans', 25)
+
 # Game variables
 hangman_status = HangmanStatus.EMPTY_ROPE
+
+# Letters variables
+letters = []
+distance_between_two_buttons = BUTTON_RADIUS * 2 + BUTTON_GAP_SIZE
+button_start_position_x = round((WINDOW_WIDTH - distance_between_two_buttons * BUTTONS_IN_ROW) / BUTTON_ROWS)
+button_start_position_y = 430
+for index in range(26):
+    position_x = button_start_position_x + BUTTON_GAP_SIZE * 2 + distance_between_two_buttons * (index % BUTTONS_IN_ROW)
+    position_y = button_start_position_y + (index // BUTTONS_IN_ROW) * distance_between_two_buttons
+    # The function "chr()" returns a character representation of the number we give it as a parameter
+    letters.append((chr(LETTER_A_ASCII_CODE + index), position_x, position_y))
 
 # Setup game loop
 clock = pygame.time.Clock()
@@ -30,11 +44,7 @@ while is_running:
     # Make sure the game loop runs at the speed we set
     clock.tick(FPS)
 
-    # Fill the background of the game window
-    WINDOW.fill(WHITE)
-    WINDOW.blit(hangman_images[int(hangman_status)], (150, 100))
-    # In Pygame we have to manually say when we want to display on the screen everything we drew so far
-    pygame.display.update()
+    draw(WINDOW, hangman_images, hangman_status, letters, LETTER_FONT)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
