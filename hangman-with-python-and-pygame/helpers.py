f3c122d2068c8f06f444f13aba6f21651a7a95db
Dominik Stefancik <dstefancik@gmail.com>
2026-01-21 18:14:02 +0100

    Implement clicking on buttons with letters

--- hangman-with-python-and-pygame/helpers.py
+++ hangman-with-python-and-pygame/helpers.py
@@ -12,11 +12,14 @@ def draw(surface, hangman_images, hangman_status, letters, font):
     surface.blit(hangman_images[int(hangman_status)], (150, 100))
 
     # Draw letters
-    for letter_tuple in letters:
-        character, x, y = letter_tuple
-        pygame.draw.circle(surface, BLACK, (x, y), BUTTON_RADIUS, BUTTON_CIRCLE_THICKNESS)
-        text = font.render(character, 1, BLACK)
-        surface.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
+    for letter_array in letters:
+        character, x, y, is_visible = letter_array
+        # If a letter is invisible, that means a player already clicked on it.
+        # In that case we don't want to show it, so the player will not be able to click on it multiple times.
+        if is_visible:
+            pygame.draw.circle(surface, BLACK, (x, y), BUTTON_RADIUS, BUTTON_CIRCLE_THICKNESS)
+            text = font.render(character, 1, BLACK)
+            surface.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
 
     # In Pygame we have to manually say when we want to display on the screen everything we drew so far
     pygame.display.update()
--- hangman-with-python-and-pygame/main.py
+++ hangman-with-python-and-pygame/main.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 from constants import BUTTON_GAP_SIZE, BUTTONS_IN_ROW, BUTTON_RADIUS, BUTTON_ROWS, FPS, LETTER_A_ASCII_CODE, WINDOW_HEIGHT, WINDOW_WIDTH
@@ -25,7 +27,9 @@ LETTER_FONT = pygame.font.SysFont('comicsans', 25)
 # Game variables
 hangman_status = HangmanStatus.EMPTY_ROPE
 
-# Letters variables
+# Each letter will be an array of items [character, x-position, y-position, is_visible]
+# We want to modify properties of these items and with using an array it is easier than using a tuple
+# (see setting up visibility after a player clicks on a letter)
 letters = []
 distance_between_two_buttons = BUTTON_RADIUS * 2 + BUTTON_GAP_SIZE
 button_start_position_x = round((WINDOW_WIDTH - distance_between_two_buttons * BUTTONS_IN_ROW) / BUTTON_ROWS)
@@ -34,7 +38,7 @@ for index in range(26):
     position_x = button_start_position_x + BUTTON_GAP_SIZE * 2 + distance_between_two_buttons * (index % BUTTONS_IN_ROW)
     position_y = button_start_position_y + (index // BUTTONS_IN_ROW) * distance_between_two_buttons
     # The function "chr()" returns a character representation of the number we give it as a parameter
-    letters.append((chr(LETTER_A_ASCII_CODE + index), position_x, position_y))
+    letters.append([chr(LETTER_A_ASCII_CODE + index), position_x, position_y, True])
 
 # Setup game loop
 clock = pygame.time.Clock()
@@ -51,8 +55,19 @@ while is_running:
            is_running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_position = pygame.mouse.get_pos()
-            print(mouse_position)
+            mouse_position_x, mouse_position_y = pygame.mouse.get_pos()
+
+            # Check if in the time when the click happned the mouse position was "inside" of any of the buttons
+            for letter_array in letters:
+                character, x, y, is_visible = letter_array
+                if is_visible:
+                    distance = math.sqrt((x - mouse_position_x) ** 2 + (y - mouse_position_y) ** 2)
+
+                    if distance < BUTTON_RADIUS:
+                        # After we clicked on a button, we will make it invisible,
+                        # so the player cannot click on it multiple times
+                        letter_array[3] = False
+
 
 
 pygame.quit()
