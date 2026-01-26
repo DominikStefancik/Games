b5a99a426adbcca707c32cecfe7b3d52c0de7f70
Dominik Stefancik <dstefancik@gmail.com>
2026-01-26 20:56:18 +0100

    Implement jumping from floor and walls

--- super-pirate-platformer-with-python-and-pygame-ce/player/constants.py
+++ super-pirate-platformer-with-python-and-pygame-ce/player/constants.py
@@ -2,7 +2,17 @@ from enum import Enum
 
 PLAYER_SPEED = 200
 PLAYER_GRAVITY = 300
+PLAYER_JUMP_HEIGHT = 400
 
 class Collision(Enum):
     VERTICAL = "vertical",
     HORIZONTAL = "horizontal"
+
+class SurfaceContact(Enum):
+    FLOOR = "floor",
+    LEFT = "left"
+    RIGHT = "right"
+
+class PlayerTimerType(Enum):
+    WALL_JUMP = "wall_jump"
+    WALL_SLIDE_BLOCK = "wall_slide_block"
--- super-pirate-platformer-with-python-and-pygame-ce/player/player.py
+++ super-pirate-platformer-with-python-and-pygame-ce/player/player.py
@@ -1,6 +1,15 @@
 from settings import pygame, vector, TILE_SIZE
+from timer import Timer
 
-from .constants import Collision, PLAYER_GRAVITY, PLAYER_SPEED
+from .constants import (
+    Collision,
+    PLAYER_GRAVITY,
+    PLAYER_JUMP_HEIGHT,
+    PLAYER_SPEED,
+    PlayerTimerType,
+    SurfaceContact,
+)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups, position, collision_sprites):
@@ -8,42 +17,115 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
         self.image.fill("red")
         # Represents a rectangle to figure out where the player will be drawn in a current frame
-        self.rect = self.image.get_frect(topleft = position)
+        self.rect = self.image.get_frect(topleft=position)
         # Represents a rectangle to figure out where the player was drawn in a previous frame
         self.previous_rect = self.rect.copy()
 
         self.direction = vector()
         self.speed = PLAYER_SPEED
+        self.is_jumping = False
+        self.is_on_surface = {
+            SurfaceContact.FLOOR: False,
+            SurfaceContact.LEFT: False,
+            SurfaceContact.RIGHT: False,
+        }
 
         self.collision_sprites = collision_sprites
+
+        self.timers = {
+            PlayerTimerType.WALL_JUMP: Timer(500),
+            # Timer for allowing a wall jump after a short time interval
+            PlayerTimerType.WALL_SLIDE_BLOCK: Timer(250),
+        }
 
     def process_key_input(self):
         keys = pygame.key.get_pressed()
         input_vector = vector()
 
-        if keys[pygame.K_LEFT]:
-            input_vector.x -= 1
-        if keys[pygame.K_RIGHT]:
-            input_vector.x += 1
-        if keys[pygame.K_UP]:
-            input_vector.y -= 1
-        if keys[pygame.K_DOWN]:
-            input_vector.y += 1
+        if not self.timers[PlayerTimerType.WALL_JUMP].active:
+            if keys[pygame.K_LEFT]:
+                input_vector.x -= 1
+            if keys[pygame.K_RIGHT]:
+                input_vector.x += 1
 
-        # Normalise the vector only if one of its values is not zero
-        self.direction.x = input_vector.normalize().x if input_vector else input_vector.x
+            # Normalise the vector only if one of its values is not zero
+            self.direction.x = (
+                input_vector.normalize().x if input_vector else input_vector.x
+            )
+
+        is_on_surface = any(
+            [
+                self.is_on_surface[SurfaceContact.FLOOR],
+                self.is_on_surface[SurfaceContact.LEFT],
+                self.is_on_surface[SurfaceContact.RIGHT],
+            ]
+        )
+
+        if keys[pygame.K_SPACE] and is_on_surface:
+            self.is_jumping = True
 
     def move(self, delta_time):
         self.rect.x += self.direction.x * self.speed * delta_time
         self.detect_collision(Collision.HORIZONTAL)
 
         # Process vertical change
-        # The longer the player keeps falling, the bigger the gravity is and the faster he falls
-        self.direction.y += PLAYER_GRAVITY / 2 * delta_time
-        self.rect.y += self.direction.y * delta_time
-        self.direction.y += PLAYER_GRAVITY / 2 * delta_time
+        is_on_wall = any(
+            [
+                self.is_on_surface[SurfaceContact.LEFT],
+                self.is_on_surface[SurfaceContact.RIGHT],
+            ]
+        )
+
+        if (
+            not self.is_on_surface[SurfaceContact.FLOOR]
+            and is_on_wall
+            and not self.timers[PlayerTimerType.WALL_SLIDE_BLOCK].active
+        ):
+            self.direction.y = 0
+            self.rect.y += PLAYER_GRAVITY / 10 * delta_time
+        else:
+            # The longer the player keeps falling, the bigger the gravity is and the faster he falls
+            self.direction.y += PLAYER_GRAVITY / 2 * delta_time
+            self.rect.y += self.direction.y * delta_time
+            self.direction.y += PLAYER_GRAVITY / 2 * delta_time
+
         self.detect_collision(Collision.VERTICAL)
 
+        if self.is_jumping:
+            if self.is_on_surface[SurfaceContact.FLOOR]:
+                self.direction.y = -PLAYER_JUMP_HEIGHT
+                # After the player jumps of the floor, he should not be able to immediately jump of the wall
+                self.timers[PlayerTimerType.WALL_SLIDE_BLOCK].activate()
+            if is_on_wall and not self.timers[PlayerTimerType.WALL_SLIDE_BLOCK].active:
+                self.timers[PlayerTimerType.WALL_JUMP].activate()
+                self.direction.x = 1 if self.is_on_surface[SurfaceContact.LEFT] else -1
+
+            self.is_jumping = False
+
+    def check_contact_with_surface(self):
+        floor_rectangle = pygame.Rect(self.rect.bottomleft, (self.rect.width, 2))
+        left_rectangle = pygame.Rect(
+            self.rect.topleft + vector(-2, self.rect.height / 4),
+            (2, self.rect.height / 2),
+        )
+        right_rectangle = pygame.Rect(
+            self.rect.topright + vector(0, self.rect.height / 4),
+            (2, self.rect.height / 2),
+        )
+        collision_rectangles = [sprite.rect for sprite in self.collision_sprites]
+
+        # The method "collidelist()" checks if the given rectangle collides with any of sprites provided
+        # in the list argument. Returns an index of the sprite with which the given rectangle collides.
+        # If the return value is -1, it doesn't collide with any of the sprites from the list.
+        self.is_on_surface[SurfaceContact.FLOOR] = (
+            floor_rectangle.collidelist(collision_rectangles) >= 0
+        )
+        self.is_on_surface[SurfaceContact.LEFT] = (
+            left_rectangle.collidelist(collision_rectangles) >= 0
+        )
+        self.is_on_surface[SurfaceContact.RIGHT] = (
+            right_rectangle.collidelist(collision_rectangles) >= 0
+        )
 
     def detect_collision(self, collision_axis):
         # For processing collisions we want to separate horizontal and vertical axes
@@ -52,32 +134,50 @@ class Player(pygame.sprite.Sprite):
                 if collision_axis == Collision.HORIZONTAL:
                     # Collision happened from the left side of the player to the right side of the sprite
                     # where the player was on the right side of the sprite before the collision
-                    if self.rect.left <= sprite.rect.right and self.previous_rect.left >= sprite.previous_rect.right:
+                    if (
+                        self.rect.left <= sprite.rect.right
+                        and self.previous_rect.left >= sprite.previous_rect.right
+                    ):
                         self.rect.left = sprite.rect.right
 
                     # Collision happened from the right side of the player to the left side of the sprite
                     # where the player was on the left side of the sprite before the collision
-                    if self.rect.right >= sprite.rect.left and self.previous_rect.right <= sprite.previous_rect.left:
+                    if (
+                        self.rect.right >= sprite.rect.left
+                        and self.previous_rect.right <= sprite.previous_rect.left
+                    ):
                         self.rect.right = sprite.rect.left
 
                 if collision_axis == Collision.VERTICAL:
                     # Collision happened from the top side of the player to the bottom side of the sprite
                     # where the player was on the bottom side of the sprite before the collision
-                    if self.rect.top <= sprite.rect.bottom and self.previous_rect.top >= sprite.previous_rect.bottom:
+                    if (
+                        self.rect.top <= sprite.rect.bottom
+                        and self.previous_rect.top >= sprite.previous_rect.bottom
+                    ):
                         self.rect.top = sprite.rect.bottom
 
                     # Collision happened from the bottom side of the player to the top side of the sprite
                     # where the player was on the top side of the sprite before the collision
-                    if self.rect.bottom >= sprite.rect.top and self.previous_rect.bottom <= sprite.previous_rect.top:
+                    if (
+                        self.rect.bottom >= sprite.rect.top
+                        and self.previous_rect.bottom <= sprite.previous_rect.top
+                    ):
                         self.rect.bottom = sprite.rect.top
 
                     # If a vertical collision happened, we want to reset the vertical direction
                     # otherwise, the gravity would keep applying even though the player is not falling
                     self.direction.y = 0
 
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+
     def update(self, delta_time):
         # Before updating the movement, store the position of the current rectangle
         # This will be then used for a collision detection
         self.previous_rect = self.rect.copy()
+        self.update_timers()
         self.process_key_input()
         self.move(delta_time)
+        self.check_contact_with_surface()
