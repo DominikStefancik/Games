656b07a8847fed55606d98e44d3cc97a0f18c6c5
Dominik Stefancik <dstefancik@gmail.com>
2026-01-29 12:10:39 +0100

    Refactor code for loading assets

---
+++ super-pirate-platformer-with-python-and-pygame-ce/loader.py
@@ -0,0 +1,83 @@
+from levels.constants import LevelObjectAssetGroup
+
+from import_helpers import *
+
+
+def load_game_graphics():
+    return {
+        LevelObjectAssetGroup.FLAG.value: import_folder(
+            "assets", "graphics", "level", "flag"
+        ),
+        LevelObjectAssetGroup.SAW.value: import_folder(
+            "assets", "graphics", "enemies", "saw", "animation"
+        ),
+        LevelObjectAssetGroup.SAW_CHAIN.value: import_image(
+            "assets", "graphics", "enemies", "saw", "saw_chain"
+        ),
+        LevelObjectAssetGroup.FLOOR_SPIKE.value: import_folder(
+            "assets", "graphics", "enemies", "floor_spikes"
+        ),
+        LevelObjectAssetGroup.PALM.value: import_subfolders_as_dict(
+            "assets", "graphics", "level", "palms"
+        ),
+        LevelObjectAssetGroup.CANDLE.value: import_folder(
+            "assets", "graphics", "level", "candle"
+        ),
+        LevelObjectAssetGroup.WINDOW.value: import_folder(
+            "assets", "graphics", "level", "window"
+        ),
+        LevelObjectAssetGroup.BIG_CHAIN.value: import_folder(
+            "assets", "graphics", "level", "big_chains"
+        ),
+        LevelObjectAssetGroup.SMALL_CHAIN.value: import_folder(
+            "assets", "graphics", "level", "small_chains"
+        ),
+        LevelObjectAssetGroup.CANDLE_LIGHT.value: import_folder(
+            "assets", "graphics", "level", "candle_light"
+        ),
+        LevelObjectAssetGroup.PLAYER.value: import_subfolders_as_dict(
+            "assets", "graphics", "player"
+        ),
+        LevelObjectAssetGroup.HELICOPTER.value: import_folder(
+            "assets", "graphics", "level", "helicopter"
+        ),
+        LevelObjectAssetGroup.BOAT.value: import_folder(
+            "assets", "graphics", "objects", "boat"
+        ),
+        LevelObjectAssetGroup.SPIKED_BALL.value: import_image(
+            "assets", "graphics", "enemies", "spiked_ball", "spiked_ball"
+        ),
+        LevelObjectAssetGroup.SPIKED_CHAIN.value: import_image(
+            "assets", "graphics", "enemies", "spiked_ball", "spiked_chain"
+        ),
+        LevelObjectAssetGroup.TOOTH.value: import_folder(
+            "assets", "graphics", "enemies", "tooth", "run"
+        ),
+        LevelObjectAssetGroup.SHELL.value: import_subfolders_as_dict(
+            "assets", "graphics", "enemies", "shell"
+        ),
+        LevelObjectAssetGroup.PEARL.value: import_image(
+            "assets", "graphics", "enemies", "bullets", "pearl"
+        ),
+        LevelObjectAssetGroup.PALM_BACKGROUND.value: import_folder(
+            "assets", "graphics", "level", "palms", "palm_bg"
+        ),
+        LevelObjectAssetGroup.PALM_BACKGROUND_LEFT.value: import_folder(
+            "assets", "graphics", "level", "palms", "palm_bg_left"
+        ),
+        LevelObjectAssetGroup.PALM_BACKGROUND_RIGHT.value: import_folder(
+            "assets", "graphics", "level", "palms", "palm_bg_right"
+        ),
+        LevelObjectAssetGroup.PALM_LEFT.value: import_folder(
+            "assets", "graphics", "level", "palms", "palm_left"
+        ),
+        LevelObjectAssetGroup.PALM_RIGHT.value: import_folder(
+            "assets", "graphics", "level", "palms", "palm_right"
+        ),
+        LevelObjectAssetGroup.PALM_SMALL.value: import_folder(
+            "assets", "graphics", "level", "palms", "palm_small"
+        ),
+        LevelObjectAssetGroup.PALM_LARGE.value: import_folder(
+            "assets", "graphics", "level", "palms", "palm_large"
+        ),
+    }
--- super-pirate-platformer-with-python-and-pygame-ce/main.py
+++ super-pirate-platformer-with-python-and-pygame-ce/main.py
@@ -5,8 +5,8 @@
 # The module allows us to load map data as Pygame surfaces
 from pytmx.util_pygame import load_pygame
 
-from import_helpers import *
-from levels.constants import LevelObjectAssetGroup, OMNI_PATH
+from loader import load_game_graphics
+from levels.constants import OMNI_PATH
 from levels.level import Level
 from settings import pygame, sys, WINDOW_HEIGHT, WINDOW_WIDTH
 
@@ -24,83 +24,7 @@ class Game:
         self.current_stage = Level(self.level_maps[0], self.level_frames)
 
     def import_assets(self):
-        self.level_frames = {
-            LevelObjectAssetGroup.FLAG.value: import_folder(
-                "assets", "graphics", "level", "flag"
-            ),
-            LevelObjectAssetGroup.SAW.value: import_folder(
-                "assets", "graphics", "enemies", "saw", "animation"
-            ),
-            LevelObjectAssetGroup.SAW_CHAIN.value: import_image(
-                "assets", "graphics", "enemies", "saw", "saw_chain"
-            ),
-            LevelObjectAssetGroup.FLOOR_SPIKE.value: import_folder(
-                "assets", "graphics", "enemies", "floor_spikes"
-            ),
-            LevelObjectAssetGroup.PALM.value: import_subfolders_as_dict(
-                "assets", "graphics", "level", "palms"
-            ),
-            LevelObjectAssetGroup.CANDLE.value: import_folder(
-                "assets", "graphics", "level", "candle"
-            ),
-            LevelObjectAssetGroup.WINDOW.value: import_folder(
-                "assets", "graphics", "level", "window"
-            ),
-            LevelObjectAssetGroup.BIG_CHAIN.value: import_folder(
-                "assets", "graphics", "level", "big_chains"
-            ),
-            LevelObjectAssetGroup.SMALL_CHAIN.value: import_folder(
-                "assets", "graphics", "level", "small_chains"
-            ),
-            LevelObjectAssetGroup.CANDLE_LIGHT.value: import_folder(
-                "assets", "graphics", "level", "candle_light"
-            ),
-            LevelObjectAssetGroup.PLAYER.value: import_subfolders_as_dict(
-                "assets", "graphics", "player"
-            ),
-            LevelObjectAssetGroup.HELICOPTER.value: import_folder(
-                "assets", "graphics", "level", "helicopter"
-            ),
-            LevelObjectAssetGroup.BOAT.value: import_folder(
-                "assets", "graphics", "objects", "boat"
-            ),
-            LevelObjectAssetGroup.SPIKED_BALL.value: import_image(
-                "assets", "graphics", "enemies", "spiked_ball", "spiked_ball"
-            ),
-            LevelObjectAssetGroup.SPIKED_CHAIN.value: import_image(
-                "assets", "graphics", "enemies", "spiked_ball", "spiked_chain"
-            ),
-            LevelObjectAssetGroup.TOOTH.value: import_folder(
-                "assets", "graphics", "enemies", "tooth", "run"
-            ),
-            LevelObjectAssetGroup.SHELL.value: import_subfolders_as_dict(
-                "assets", "graphics", "enemies", "shell"
-            ),
-            LevelObjectAssetGroup.PEARL.value: import_image(
-                "assets", "graphics", "enemies", "bullets", "pearl"
-            ),
-            LevelObjectAssetGroup.PALM_BACKGROUND.value: import_folder(
-                "assets", "graphics", "level", "palms", "palm_bg"
-            ),
-            LevelObjectAssetGroup.PALM_BACKGROUND_LEFT.value: import_folder(
-                "assets", "graphics", "level", "palms", "palm_bg_left"
-            ),
-            LevelObjectAssetGroup.PALM_BACKGROUND_RIGHT.value: import_folder(
-                "assets", "graphics", "level", "palms", "palm_bg_right"
-            ),
-            LevelObjectAssetGroup.PALM_LEFT.value: import_folder(
-                "assets", "graphics", "level", "palms", "palm_left"
-            ),
-            LevelObjectAssetGroup.PALM_RIGHT.value: import_folder(
-                "assets", "graphics", "level", "palms", "palm_right"
-            ),
-            LevelObjectAssetGroup.PALM_SMALL.value: import_folder(
-                "assets", "graphics", "level", "palms", "palm_small"
-            ),
-            LevelObjectAssetGroup.PALM_LARGE.value: import_folder(
-                "assets", "graphics", "level", "palms", "palm_large"
-            ),
-        }
+        self.level_frames = load_game_graphics()
 
     def run(self):
         while True:
