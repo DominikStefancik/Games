7a517af280c1a3e845450c21cc304a3182c7452a
Dominik Stefancik <dstefancik@gmail.com>
2026-07-28 08:42:48 +0200

    Implement screen shake

--- rust/snake-with-rust-and-bevy/src/plugins/camera/mod.rs
+++ rust/snake-with-rust-and-bevy/src/plugins/camera/mod.rs
@@ -1,13 +1,33 @@
-use bevy::app::{App, Plugin, Startup};
+use bevy::{
+    app::{App, Plugin, Startup, Update},
+    state::state::OnEnter,
+    time::{Timer, TimerMode},
+};
 
-use crate::plugins::camera::systems::spawn_camera;
+use crate::plugins::{
+    camera::{
+        resources::ScreenShake,
+        systems::{apply_screenshake, reset_screenshake_timer, spawn_camera},
+    },
+    shared::GameState,
+};
 
+mod resources;
 mod systems;
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+        // Initialize the screenshake timer as already finished so it doesn't
+        // fire on startup. We reset it manually when the player dies.
+        app.insert_resource(ScreenShake({
+            let mut timer = Timer::from_seconds(0.3, TimerMode::Once);
+            timer.finish();
+            timer
+        }))
+        .add_systems(OnEnter(GameState::GameOver), reset_screenshake_timer)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, apply_screenshake);
     }
 }
--- rust/snake-with-rust-and-bevy/src/plugins/camera/systems.rs
+++ rust/snake-with-rust-and-bevy/src/plugins/camera/systems.rs
@@ -1,5 +1,49 @@
-use bevy::{camera::Camera2d, ecs::system::Commands};
+use bevy::{
+    camera::Camera2d,
+    ecs::{
+        query::With,
+        system::{Commands, Res, ResMut, Single},
+    },
+    math::Vec3,
+    time::Time,
+    transform::components::Transform,
+};
+
+use crate::plugins::camera::resources::ScreenShake;
 
 pub fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+/*
+ * Because the timer starts pre-finished (we called t.finish() in "insert_resource"),
+ * this system runs every frame doing nothing until a death occurs and shake.0.reset() is called.
+ */
+pub fn apply_screenshake(
+    time: Res<Time>,
+    mut camera_transform: Single<&mut Transform, With<Camera2d>>,
+    mut screenshake: ResMut<ScreenShake>,
+) {
+    screenshake.0.tick(time.delta());
+
+    if screenshake.0.is_finished() {
+        if camera_transform.translation != Vec3::ZERO {
+            camera_transform.translation = Vec3::ZERO;
+        }
+    } else {
+        /*
+         * "screenshake.0.fraction()" goes from 0.0 to 1.0 as time passes, so 1.0 - fraction() starts at 1.0 and decays to 0.0.
+         * Multiplying by 10.0 gives a starting intensity of 10 pixels that eases to zero.
+         * When finished, the camera is snapped back to Vec3::ZERO
+         */
+        let intensity = 10. * (1. - screenshake.0.fraction());
+        camera_transform.translation = Vec3::new(
+            rand::random::<f32>() * 2. * intensity - intensity,
+            rand::random::<f32>() * 2. * intensity - intensity,
+            0.,
+        );
+    }
+}
+
+pub fn reset_screenshake_timer(mut screenshake: ResMut<ScreenShake>) {
+    screenshake.0.reset();
 }
